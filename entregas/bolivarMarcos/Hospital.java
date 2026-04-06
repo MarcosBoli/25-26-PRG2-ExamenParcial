f3ad0151e2e483afbfd996743d32404e5fe32b85
Cyber <marcosbolivar2007@gmail.com>
2026-04-06 17:01:46 +0200

    Completada calse Hospital

--- entregas/bolivarMarcos/Hospital.java
+++ entregas/bolivarMarcos/Hospital.java
@@ -1,3 +1,18 @@
 public class Hospital{
+    private String nombreHospital;
+    private Paciente paciente;
+    private Medico medico;
+    private Consulta consulta;
+
+    public Hospital (String nombreHospital, Paciente paciente, Medico medico, Consulta consulta){
+        this.nombreHospital = nombreHospital;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.consulta = consulta;
+    }
+
+    public Hospital registrarPaciente(Paciente nuevo){
+        return new Hospital(this.nombreHospital, nuevo, this.medico, this.consulta);
+    }
     
 }
