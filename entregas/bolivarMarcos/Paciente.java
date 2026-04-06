6ad85112eabc5c014654e5aa92415194d553afb3
Cyber <marcosbolivar2007@gmail.com>
2026-04-06 16:45:27 +0200

    Completada clase Paciente

--- entregas/bolivarMarcos/Paciente.java
+++ entregas/bolivarMarcos/Paciente.java
@@ -1,3 +1,20 @@
 public class Paciente{
-    
+    private String nombre;
+    private String DNI;
+    private boolean historialMedico;
+
+    public Paciente (String nombre, String DNI, boolean historialMedico){
+        this.nombre=nombre;
+        this.DNI=DNI;
+        this.historialMedico=historialMedico;
+    }
+
+
+    public String verHistorial(){
+        if(this.historialMedico == false){
+            return "El paciente no tiene historial médico";
+        }else {
+            return "El paciente acude en periodos regulares por enfermedad crónica.";
+        }
+    }
 }
