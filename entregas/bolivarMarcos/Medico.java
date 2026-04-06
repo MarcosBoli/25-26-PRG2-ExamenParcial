d82b14c7344ab24b51c81134f5c0efc2c8be6b88
Cyber <marcosbolivar2007@gmail.com>
2026-04-06 16:47:43 +0200

    Completada clase Medico

--- entregas/bolivarMarcos/Medico.java
+++ entregas/bolivarMarcos/Medico.java
@@ -1,3 +1,12 @@
 public class Medico {
-    
+
+    private String nombre;
+    private String especialidad;
+    private String horario; 
+
+    public Medico (String nombre, String especialidad, String horario){
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.horario = horario;
+    }
 }
