12d263e152cf5a26c679a34020e806a77b8fbc42
Cyber <marcosbolivar2007@gmail.com>
2026-04-06 16:56:07 +0200

    Completada clase Consulta

--- entregas/bolivarMarcos/Consulta.java
+++ entregas/bolivarMarcos/Consulta.java
@@ -1,3 +1,26 @@
 public class Consulta{
-    
+
+    private String fecha;
+    private String diagnostico;
+    private String tratamiento;
+    private Medico mAsignado;
+    private Paciente pAsignado;
+
+    public Consulta(String fecha, String diagnostico, String tratamiento, Medico mAsignado, Paciente pAsignado){
+        this.fecha = fecha;
+        this.diagnostico = diagnostico;
+        this.tratamiento = tratamiento;
+        this.mAsignado = mAsignado;
+        this.pAsignado = pAsignado;
+    }
+
+    public consulta crearConsulta(Paciente paciente, Medico medico, String fecha){
+        return new Consulta(fecha, "Pendiente", "Pendiente", medico,paciente);
+    }
+
+    public String registrarDyT(String diagnostico, Stringg tratamiento){
+        this.diagnostico = diagnostico;
+        this.tratamiento = tratamiento;
+        return "Diagnostico: "+diagnostico+"; Tratamiento: "+tratamiento;
+    }
 }
